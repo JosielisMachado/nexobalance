d529257dbee465b739c708578507c190a558c2a0
Josielis Machado <josielisuruguay@gmail.com>
2026-07-20 02:42:16 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='nexobalance-v2.10.0';
+const CACHE='nexobalance-v2.10.1';
 const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png','https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2.45.4/dist/umd/supabase.min.js'];
 
 self.addEventListener('install',e=>{
